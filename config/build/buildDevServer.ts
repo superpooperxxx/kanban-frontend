8c90fcb5427ba6aeab8db977e41a588959f4d72f
Kyrylo Haiduk <kyrylo@talentpath.co>
2024-04-08 14:07:31 +0300

    webpack config

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,17 @@
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+import { BuildOptions } from "./types/config";
+
+export const buildDevServer = (
+  options: BuildOptions
+): DevServerConfiguration => {
+  const { port, isDev } = options;
+
+  return isDev
+    ? {
+        port,
+        open: true,
+        historyApiFallback: true,
+      }
+    : undefined;
+};
